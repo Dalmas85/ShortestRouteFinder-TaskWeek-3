eab350f352baa1dd25ea36c680adf2d3e77e0557
Dalmas85 <d6690JB@gmail.com>
2024-11-08 11:22:44 +0100

    Added new file in Develop branch

---
+++ ShortestRouteFinder/Routes.cs
@@ -0,0 +1,14 @@
+﻿public class Route
+{
+    public string StartingPoint { get; set; }
+    public string Destination { get; set; }
+    public double Distance { get; set; }
+
+    public Route(string startingPoint, string destination, double distance)
+    {
+        StartingPoint = startingPoint;
+        Destination = destination;
+        Distance = distance;
+
+    }
+}
